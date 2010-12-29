3cab1b4c1ba0e500db9ffbd1a07ca86f729a6b42
James Mead <james@floehopper.org>
2010-12-29 18:38:32 +0000

    Use newer version of floehopper-deploy which renames floehopper::static_website to floehopper::webby_app.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ server 'argonaut.slice', :app, :web, :db, :primary => true
 set :environment, 'production'
 
 manifest :app, %{
-  floehopper::static_website {'sparkymagic.com':
+  floehopper::webby_app {'sparkymagic.com':
     deploy_to => "<%= deploy_to %>",
     domain => 'sparkymagic.com'
   }
